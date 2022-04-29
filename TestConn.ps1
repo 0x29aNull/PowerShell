665309bac24db855ac5f83c788a0deec2f1671a5
0x29a Null <78053467+0x29aNull@users.noreply.github.com>
2022-04-29 08:26:26 -0700

    Update TestConn.ps1

--- TestConn.ps1
+++ TestConn.ps1
@@ -1,8 +1,12 @@
-# TestConn.ps1
-# Certain Oracle Computers lose their Network Interface Card, This solves that problem.
+# TestConn.ps1 - Script 1 or 2, Use with Reconnect.ps1
+
+# Oracle Simphony Micros Computers lose their NICs randomly, This solves that problem.
 # Create a Task Scheduler job to run this script with highest priviledges every half hour.
 # The task command should be "powershell.exe -WindowStyle hidden C:\NIC\TestConn.ps1"
 # Place this script and the Reconnect.ps1 in a folder named "NIC" on the root of C drive.
+# Once the script is ran, It will check for a connection, If that connection fails it will
+# close the Micros application, Drop the NIC, Enable the NIC, Reconnect then re-launch the
+# Simphony application.
 
 $Connection = Test-Connection -ComputerName SERVER -Count 1 -Quiet
 
