dc329c731fc1c4e28312496e9a13613a915bc6d8
0x29a Null <78053467+0x29aNull@users.noreply.github.com>
2022-05-10 08:19:48 -0700

    Create PcOuMap.ps1

---
+++ PcOuMap.ps1
@@ -0,0 +1,16 @@
+# Script to map the path of an Active Directory Computer.
+# mlogsdon 2022
+
+Import-Module ActiveDirectory
+$PC = Read-Host "Enter PC Name: "
+$Cmd = Get-ADComputer $PC
+[array]$Array = $Cmd -Split(',').ForEach({$_.Split('=')[-1]})
+[array]::Reverse($Array)
+
+foreach ($item in $Array) {
+    if ($item -eq 'DC=com') {
+        [array]$NewArray += $Array -notmatch 'DC=com'
+    }
+}
+$NewArray.Count
+$NewArray -join "`n->"
